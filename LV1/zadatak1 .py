24a418d83600b4b04144ad7b04eede2c7cc777c2
benjaminmontibeler <benjaminmontibeller@gmail.com>
2021-03-11 14:48:46 +0100

    zadatak1

---
+++ LV1/zadatak1 .py
@@ -0,0 +1,6 @@
+radni_sati = float(input("Unesite broj radnih sati: "))
+placa = float(input("Unesite iznos zarade po radnom satu: "))
+
+print("Radni sati: ", radni_sati)
+print("kn/h: ", placa)
+print("Ukupno: ", (radni_sati * placa))
