f381e15a9b2ca2cae41939ab2a4541af11485103
Benjamin Montibeler <benjaminmontibeler@gmail.com>
2021-03-25 16:28:25 +0100

    lv2

--- LV2/zadatak2.py
+++ LV2/zadatak2.py
@@ -1,33 +1,25 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 x = []
-np.random.seed(56)
+np.random.seed(562)
 for brojbacanja in range(0, 100):
-    brojbacanja = np.random.randint(low = 1, high = 6)
+    brojbacanja = np.random.randint(low = 1, high = 7)
     x.append(brojbacanja)
 
-b = np.array(x)
-print(x)
+hist,bin_edges = np.histogram(x)
 
-his = np.histogram(b, bins=range(5))
-fig, ax = plt.subplots()
-offset = 6
-plt.bar(his[1][1:],his[0])
-ax.set_xticks(his[1][1:] + offset)
-ax.set_xticklabels( ('1', '2', '3', '4', '5', '6') )
+plt.figure(figsize=[10,8])
+bin_edges = np.round(bin_edges,0)
 
-#n = 6
-#bins=np.arange(0,n+2,1)
-#a = np.arange(6)
-#fig, ax = plt.subplots(1,1)
-#ax.set_xticks(bins[:-1])
-#ax.hist(b, bins =(6), rwidth=0.8, align = 'left')
-#plt.hist(a, bins=range(min(a), max(a) + 1))
-#ax.hist(a, bins=bins, align='left')
+plt.bar(bin_edges[:-1], hist, width = 0.5, color='#0504aa',alpha=0.7)
+plt.xlim(min(bin_edges), max(bin_edges))
+plt.grid(axis='y', alpha=0.75)
+plt.xlabel('Number',fontsize=15)
+plt.ylabel('Frequency',fontsize=15)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
 plt.ylim(ymin = 0, ymax = 100)
 plt.xlim(xmin = 0, xmax = 7)
-plt.title("Histogram")
+plt.ylabel('Frequency',fontsize=15)
+plt.title('Histogram',fontsize=15)
 plt.show()
-
-
-
