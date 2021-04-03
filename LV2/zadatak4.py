1373396b7da0b7d8a7633f4ee9131ac6061db5fd
Benjamin Montibeler <benjaminmontibeler@gmail.com>
2021-04-03 22:42:37 +0200

    lv2

--- LV2/zadatak4.py
+++ LV2/zadatak4.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 import skimage.io
+from skimage.transform import rescale, resize
+
  
 img = skimage.io.imread('tiger.png', as_gray=True)
 height, width = img.shape
 
+#a)
 #matrica = np.ones((640, 960)) * 150
 #img = img + matrica
 #for i in range(0, height):
@@ -12,6 +15,7 @@ height, width = img.shape
         #if img[i][j] > 255:
             #img[i][j] = 255
 
+#b)
 #img90 = np.zeros((width, height))
 #for j in range(0, height):
     #img90[:, height - j - 1] = img[j, :]
@@ -19,13 +23,24 @@ height, width = img.shape
 #[:, 1] stupac
 #[1, :] redak
 
-imgmirror = np.zeros((height, width))
+#c)
+#imgmirror = np.zeros((height, width))
 
-for j in range(0, width):
-    imgmirror[:, width - j - 1] = img[:, j]
+#for j in range(0, width):
+    #imgmirror[:, width - j - 1] = img[:, j]
 
+#d)
+#resized_image = resize(img, (height/10, width/10))
+#rescaled_image = 255 * resized_image
+#final_image = rescaled_image.astype(np.uint8)
 
+#e)
+new_image = np.zeros((height, width))
+for i in range(0, height):
+    for j in range(0, width):
+        if j >= (width/4) and j <= (2*width/4):
+            new_image[i][j] = img[i][j]
 
 plt.figure(1) 
-plt.imshow(imgmirror, cmap='gray', vmin=0, vmax=255) 
+plt.imshow(new_image, cmap='gray', vmin=0, vmax=255) 
 plt.show()
