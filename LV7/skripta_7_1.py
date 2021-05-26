ca8e24ef6690e552bf63e308ee4523224753f8b6
Benjamin Montibeler <benjaminmontibeler@gmail.com>
2021-05-26 17:53:18 +0200

    lv7

--- LV7/skripta_7_1.py
+++ LV7/skripta_7_1.py
@@ -1,10 +1,11 @@
 import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix
-
-
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 # Model / data parameters
 num_classes = 10
 input_shape = (28, 28, 1)
@@ -43,15 +44,22 @@ y_test_s = keras.utils.to_categorical(y_test, num_classes)
 
 # TODO: kreiraj model pomocu keras.Sequential(); prikazi njegovu strukturu
 
-model = keras.Sequental([keras.input(shape = input_shape), layers.conv2d(32, kernel_size=(3, 3), activation = 'relu'), 
-layers.MaxPooling2D(pool_size = (2, 2)), layers.Flatten(), layers.Dense(10, activation = 'softmax')])
+model = keras.Sequential([
+    keras.Input(shape=input_shape),
+    layers.Conv2D(32,kernel_size=(3,3),activation='relu'),
+    layers.MaxPooling2D(pool_size=(2,2)),
+    layers.Conv2D(16,kernel_size=(3,3),activation='relu'),
+    layers.MaxPooling2D(pool_size=(2,2)),
+    layers.Flatten(),
+    layers.Dense(10,activation='softmax'),
+])
 model.summary()
 
 
 # TODO: definiraj karakteristike procesa ucenja pomocu .compile()
 
+#model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-
 
 
 # TODO: provedi ucenje mreze
@@ -61,8 +69,9 @@ model.fit(x_train_s, y_train_s, epochs=5, batch_size=32)
 
 # TODO: Prikazi test accuracy i matricu zabune
 
-loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
+loss_and_metrics = model.evaluate(x_test_s, y_test_s, batch_size=128)
+print(loss_and_metrics)
 
 # TODO: spremi model
 
-classes = model.predict(x_test, batch_size=128)
+model.save('my_model')
